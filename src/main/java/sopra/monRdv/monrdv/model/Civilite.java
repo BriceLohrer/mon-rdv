24acba0193d3c365bc35b553f20211c9f8c61f39
Cécile <cecile.larrouy@outlook.fr>
2020-10-27 12:26:42 +0100

    commit model

---
+++ src/main/java/sopra/monRdv/monrdv/model/Civilite.java
@@ -0,0 +1,17 @@
+package sopra.allodoc.model;
+
+public enum Civilite {
+	
+	M("Monsieur"), MME("Madame"), MLLE("Mademoiselle");
+
+	private final String label;
+
+	private Civilite(String label) {
+		this.label = label;
+	}
+
+	public String getLabel() {
+		return label;
+	}
+	
+}
